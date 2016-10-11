5879b0d77f59e13b07a2a4b12b169d57b6f9686c
Anthony Washington <awashington251@gmail.com>
2016-10-11 16:55:54 -0500

    Added some parsing finally! :)

--- parse/resource.py
+++ parse/resource.py
@@ -15,8 +15,37 @@ class ArgumentParser(argparse.ArgumentParser):
             default=None, help="The path to the upgrade test directory.")
 
 
+def parse_data(sub_file):
+    output = sub_file.splitlines()[1:]
+
+    results = []
+
+    for row in output:
+        data = row.split(',')
+
+        # strip data from each row
+        test = data[0].split('[')[0].split('.')[-2]
+        status = data[1]
+        start = data[2]
+        stop = data[3]
+
+        results.append({
+            'test-name': test,
+            'status': status,
+            'start': start,
+            'stop': stop
+        })
+    return results
+
+
 def entry_point():
     cls_args = ArgumentParser().parse_args()
+    data = []
     for _file in os.listdir(cls_args.upgrade_dir):
-        acrap = open(os.path.join(cls_args.upgrade_dir, _file))
-        print acrap.read()
+        result = parse_data(open(os.path.join(cls_args.upgrade_dir,
+                                              _file)).read())
+        if result:
+            data.append(result)
+
+    print result
+
